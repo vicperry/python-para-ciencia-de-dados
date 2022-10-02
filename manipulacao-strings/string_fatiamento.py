217c9d0d910d0ad45ddbee77c6eff3a4e4d4ed6e
vicperry <vic.hugx@gmail.com>
2022-10-02 19:00:54 -0300

    Manipulação de string.

---
+++ manipulacao-strings/string_fatiamento.py
@@ -0,0 +1,10 @@
+nome = 'Alice da Silva Xavier'
+#[start:end:step]
+print(nome[0]) #indice
+print(nome[:9]) #do começo até o índice 9
+print(nome[10:]) #do indice ao fim
+print(nome[10:10]) #do indice ao indice
+print(nome[10:10:2]) #do indice ao indice a cada 2 passos
+print(nome[:]) #do inicio ao fim
+print(nome[::-1]) #o texto ao contrário, pois o passo 1 está negativo
+
---
+++ manipulacao-strings/string_multiplas_linhas.py
@@ -0,0 +1,9 @@
+nome = 'Kleber'
+
+mensagem = f"""
+    Oi, meu nome é {nome},
+EU tô aprendendo Python.
+        Essamensagem tem diferentes recuos.
+"""
+
+print(mensagem)
